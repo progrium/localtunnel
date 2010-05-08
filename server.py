628ad82fe222845d69ad51618fabdfa824734c15
Jeff Lindsay <progrium@gmail.com>
2010-05-07 22:26:01 -0700

    starting over, doing it right

--- server.py
+++ server.py
@@ -1,70 +1,82 @@
 from twisted.internet import protocol, reactor, defer, task
 from twisted.protocols import basic
 from twisted.python import log
-from twisted.web import http
+from twisted.web import http, proxy, resource, server
 import sys, time
+from urllib import quote as urlquote
 
-# Debug thing
-FIXED_TUNNEL_NAME = True
+import urlparse
+import socket
+import time
 
-tunnels = {}
+HOSTNAME = 'localtunnel.com'
+SSH_USER = 'root'
+PORT_RANGE = [9000, 9100]
+
+TUNNELS = {}
+
+def find_port():
+    port = PORT_RANGE[0]
+    start_time = time.time()
+    while not port_available(port):
+        if time.time()-start_time > 3:
+            raise Exception("No port available")
+        port += 1
+        if port >= PORT_RANGE[1]: port = PORT_RANGE[0]
+    return port
+
+def port_available(port):
+    try:
+        socket.create_connection(['127.0.0.1', port]).close()
+        return False
+    except socket.error:
+        return True
+    
 
 def baseN(num,b=36,numerals="0123456789abcdefghijklmnopqrstuvwxyz"): 
     return ((num == 0) and  "0" ) or (baseN(num // b, b).lstrip("0") + numerals[num % b])
 
-class IncomingChannel(basic.LineReceiver):
-    tunnel = None
-    buffer = []
+class LocalReverseProxy(proxy.ReverseProxyResource):
+    def __init__(self, path='', host='127.0.0.1'):
+        proxy.ReverseProxyResource.__init__(self, host, None, path)
     
-    def lineReceived(self, line):
-        if not self.tunnel:
-            self.buffer.append(line)
-            if line.lower().startswith("host: "):
-                host = line.lower().split(': ')[1]
-                if host in tunnels:
-                    self.tunnel = tunnels[host]
-                    self.tunnel.incoming = self
-                    for l in self.buffer:
-                        self.tunnel.send(l + '\r\n')
-                else:
-                    self.transport.loseConnection()
+    def getChild(self, path, request):
+        return LocalReverseProxy(self.path + '/' + urlquote(path, safe=""), host=self.host)
+    
+    def render(self, request):
+        tunnel_host = request.getHeader('host').split(':')[0]
+        if not tunnel_host in TUNNELS: return "Not found"
+        
+        request.content.seek(0, 0)
+        qs = urlparse.urlparse(request.uri)[4]
+        if qs:
+            rest = self.path + '?' + qs
         else:
-            self.tunnel.send(line + '\r\n')
+            rest = self.path
+        clientFactory = self.proxyClientFactoryClass(
+            request.method, rest, request.clientproto,
+            request.getAllHeaders(), request.content.read(), request)
+        self.reactor.connectTCP(self.host, TUNNELS[tunnel_host], clientFactory)
+        return server.NOT_DONE_YET
+
+class TunnelResource(resource.Resource):
+    isLeaf = True
     
-    def rawDataReceived(self, data):
-        self.tunnel.send(data)
-    
-    def connectionMade(self):
-        self.reqId = hash(time.time())
-        self.buffer = ['^^CONNECT--']
-    
-    def connectionLost(self, reason):
-        if self.tunnel:
-            self.tunnel.send('^^CLOSE--\r\n')
-
-
-class IncomingFactory(protocol.ServerFactory):
-    protocol = IncomingChannel
-
-class TunnelServerProtocol(protocol.Protocol):
-    incoming = None
-    
-    def dataReceived(self, data):
-        self.incoming.transport.write(data)
-
-    def connectionMade(self):
-        host = 'localhost:8999' if FIXED_TUNNEL_NAME else '%s.localhost:8999' % baseN(abs(hash(time.time())))
-        print "!!! Use hostname %s !!!" % host
-        tunnels[host] = self
-        
-    def send(self, data):
-        self.transport.write('%s|%s' % (self.incoming.reqId, data))
-        
-
-class TunnelServerFactory(protocol.ServerFactory):
-    protocol = TunnelServerProtocol
+    def render_POST(self, request):
+        if request.path == '/open':
+            port = find_port()
+            tunnel_host = '%s.%s' % (baseN(port), HOSTNAME)
+            TUNNELS[tunnel_host] = port
+            return "ssh -NR %s:localhost:PORT %s@%s\n" % (port, SSH_USER, tunnel_host)
+        elif request.path == '/close':
+            tunnel_host = request.args.get('host', [None])[0]
+            if tunnel_host:
+                del TUNNELS[tunnel_host]
+            return "OK"
+        else:
+            return "huh?"
 
 log.startLogging(sys.stdout)
-reactor.listenTCP(8777, TunnelServerFactory())
-reactor.listenTCP(8999, IncomingFactory())
+reactor.listenTCP(8080, server.Site(LocalReverseProxy()))
+reactor.listenTCP(8888, server.Site(TunnelResource()))
 reactor.run()
