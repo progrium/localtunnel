9e5f65f441518770269020b1b9e947ee9e84e5d8
Aaron Lerch <aaronlerch@gmail.com>
2011-09-05 22:24:59 -0400

    Assume classes are suffixed with AutoConfig. Only look up filenames matching the specified argument. Underscore class names (borrowed from Rails.)

--- lib/localtunnel/autoconfig.rb
+++ lib/localtunnel/autoconfig.rb
@@ -1,5 +1,15 @@
 module LocalTunnel
   module AutoConfig
+    class Base
+      def self.inherited(base)
+        super
+
+        if base.name && base.name !~ /Base$/
+          LocalTunnel::AutoConfig.subclasses << base
+        end
+      end
+    end
+
     def self.subclasses
       @subclasses ||= []
     end
@@ -7,7 +17,7 @@ module LocalTunnel
     def self.find(name)
       lookup(name)
 
-      names = Hash[subclasses.map { |klass| [klass.respond_to?(:autoconfig_name) ? klass.autoconfig_name.downcase : klass.name.downcase, klass] }]
+      names = Hash[subclasses.map { |klass| [autoconfig_name(klass).downcase, klass] }]
       klass = names[name]
       return nil if klass.nil?
 
@@ -19,28 +29,40 @@ module LocalTunnel
       end
     end
 
-    def self.lookup(name)
-      $LOAD_PATH.each do |base|
-        Dir[File.join(base, "localtunnel", "**", "#{name}_autoconfig.rb")].each do |path|
-          begin
-            path = path.sub("#{base}/", "")
-            require path
-          rescue Exception
-            # No problem
+    private
+
+      def self.autoconfig_name(klass)
+        if klass.name =~ /(\w*)AutoConfig$/
+          $1.underscore
+        else
+          klass.name
+        end
+      end
+
+      def self.lookup(name)
+        $LOAD_PATH.each do |base|
+          Dir[File.join(base, "localtunnel", "#{name}_autoconfig.rb")].each do |path|
+            begin
+              path = path.sub("#{base}/", "")
+              require path
+            rescue Exception => e
+              puts "   [WARNING] Could not load autoconfig #{path.inspect}. Error: #{e.message}.\n#{e.backtrace.join("\n")}"
+            end
           end
         end
       end
-    end
-  end
 
-  class Base
-    def self.inherited(base)
-      super
-
-      if base.name && base.name !~ /Base$/
-        LocalTunnel::AutoConfig.subclasses << base
-      end
-    end
   end
 end
 
+class String
+  def underscore
+    word = self.dup
+    word.gsub!(/::/, '/')
+    word.gsub!(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
+    word.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
+    word.tr!("-", "_")
+    word.downcase!
+    word
+  end
+end
